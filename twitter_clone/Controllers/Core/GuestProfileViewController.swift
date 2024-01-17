5f51ccfe9f348520fdc2ec8a3e5b4e73fcad15c5
maliyahya <yahyaoglu.muhammet@std.izu.edu.tr>
2024-01-17 10:48:17 +0300

    searchAdded

---
+++ twitter_clone/Controllers/Core/GuestProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GuestProfileViewController.swift
+//  twitter_clone
+//
+//  Created by Muhammet Ali Yahyaoğlu on 16.01.2024.
+//
+
+import UIKit
+
+class GuestProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ twitter_clone/Utils/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  twitter_clone
+//
+//  Created by Muhammet Ali Yahyaoğlu on 16.01.2024.
+//
+
+import Foundation
